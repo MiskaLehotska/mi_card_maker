2bff4b64744de18e0e2d46a51498e261b7a9f81a
Miska Lehotska <misela.le@gmail.com>
2022-08-21 11:45:27 +0200

    innitial commit

---
+++ src/main/java/sk/game_helper/mi_card_maker/App.java
@@ -0,0 +1,13 @@
+package sk.game_helper.mi_card_maker;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

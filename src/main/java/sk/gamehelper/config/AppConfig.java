ccb9e9ddc9e6be4e2bca3e0f5a902d8819f3b841
Martin Krajcovic <mar.krajc@gmail.com>
2022-08-23 18:33:42 +0200

    AppConfig - added @Bean for DataSource connection object

--- src/main/java/sk/gamehelper/config/AppConfig.java
+++ src/main/java/sk/gamehelper/config/AppConfig.java
@@ -1,8 +1,29 @@
 package sk.gamehelper.config;
 
-/**
- * This will be the base spring application configuration class
- */
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@ComponentScan(basePackages = "sk.gamehelper")
+@PropertySource("classpath:/db/connection.properties")
 public class AppConfig {
+
+	@Bean
+	public DataSource getDatabaseDataSource(
+			@Value("${url}") String url, 
+			@Value("${username}") String username, 
+			@Value("${password}") String password) {
+
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setUrl(url);
+		dataSource.setUsername(username);
+		dataSource.setPassword(password);
+
+		return dataSource;
+	}
 
 }

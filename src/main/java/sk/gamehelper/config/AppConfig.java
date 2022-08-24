80e45efafcdab7a9bf66a4812ec7e6a945afcf8c
Martin Krajcovic <mar.krajc@gmail.com>
2022-08-24 23:17:41 +0200

    added single reusable connection datasource/jdbcTemplate bean + transaction management

--- src/main/java/sk/gamehelper/config/AppConfig.java
+++ src/main/java/sk/gamehelper/config/AppConfig.java
@@ -2,15 +2,31 @@ package sk.gamehelper.config;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.SingleConnectionDataSource;
+import org.springframework.transaction.TransactionManager;
 
 @ComponentScan(basePackages = "sk.gamehelper")
 @PropertySource("classpath:/db/connection.properties")
 public class AppConfig {
+
+	@Bean
+	@Autowired
+	public TransactionManager getTransactionManager(DataSource dataSource) {
+		return new DataSourceTransactionManager(dataSource);
+	}
+
+	@Bean
+	@Autowired
+	public JdbcTemplate getJdbcTemplate(DataSource dataSource) {
+		return new JdbcTemplate(dataSource);
+	}
 
 	@Bean
 	public DataSource getDatabaseDataSource(
@@ -18,12 +34,11 @@ public class AppConfig {
 			@Value("${username}") String username, 
 			@Value("${password}") String password) {
 
-		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		SingleConnectionDataSource dataSource = new SingleConnectionDataSource();
 		dataSource.setUrl(url);
 		dataSource.setUsername(username);
 		dataSource.setPassword(password);
 
 		return dataSource;
 	}
-
 }

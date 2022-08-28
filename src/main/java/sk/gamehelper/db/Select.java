169a094acb99f8b1a00a966bbef5f577b5d0d4bc
Martin Krajcovic <mar.krajc@gmail.com>
2022-08-28 09:35:16 +0200

    Select - added overloaded where methods + concatenating where clauses at end

--- src/main/java/sk/gamehelper/db/Select.java
+++ src/main/java/sk/gamehelper/db/Select.java
@@ -7,20 +7,31 @@ import java.util.logging.Logger;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 // TODO: add documentation with hints
-public class Select {
+public final class Select {
 
 	private static final Logger logger = Logger.getAnonymousLogger();
 
 	private JdbcTemplate jdbcTemplate;
 	private StringBuilder selectBuilder;
+	private StringBuilder whereBuilder;
 	private boolean isWhereClauseApplied;
 	private int requestedColumnsCount;
 
 	Select(JdbcTemplate jdbcTemplate, String... columns) {
 		this.jdbcTemplate = jdbcTemplate;
 		this.selectBuilder = new StringBuilder("SELECT ");
-		requestedColumnsCount = columns.length;
+		this.whereBuilder = new StringBuilder();
+		this.requestedColumnsCount = columns.length;
 		select(columns);
+	}
+
+	// toto mi vobec nepomoze pri Join triede
+	public Select(Select otherSelect) {
+		this.jdbcTemplate = otherSelect.jdbcTemplate;
+		this.selectBuilder = new StringBuilder(otherSelect.selectBuilder);
+		this.whereBuilder = new StringBuilder(otherSelect.whereBuilder);
+		this.isWhereClauseApplied = otherSelect.isWhereClauseApplied;
+		this.requestedColumnsCount = otherSelect.requestedColumnsCount;
 	}
 
 	private Select select(String... columns) {
@@ -38,60 +49,68 @@ public class Select {
 		return this;
 	}
 
-	// TODO: these wheres should be appended at the total end because of joins !!
+	// TODO: these wheres should be appended at the total end because of joins
 	public Select where(String column, Object value) {
 		appendWhereClauseConnector();
-		selectBuilder.append(column);
-		selectBuilder.append(" = ");
+		whereBuilder.append(column);
+		whereBuilder.append(" = ");
 		if (value instanceof CharSequence) {
 			value = "'" + value + "'";
 		}
-		selectBuilder.append(value);
+		whereBuilder.append(value);
+		return this;
+	}
+
+	public Select where(String column, Select innerSelect) {
+		appendWhereClauseConnector();
+		whereBuilder.append(column);
+		whereBuilder.append(" = (");
+		whereBuilder.append(innerSelect.selectBuilder);
+		whereBuilder.append(")");
+		return this;
+	}
+
+	public Select where(String customWhere) {
+		appendWhereClauseConnector();
+		if (customWhere.contains("WHERE ")) {
+			customWhere = customWhere.replace("WHERE ", "");
+		}
+		whereBuilder.append(customWhere);
 		return this;
 	}
 
 	private void appendWhereClauseConnector() {
 		if (isWhereClauseApplied) {
-			selectBuilder.append(" AND ");
+			whereBuilder.append(" AND ");
 		} else {
-			selectBuilder.append(" WHERE ");
+			whereBuilder.append(" WHERE ");
 			isWhereClauseApplied = true;
 		}
-	}
-
-	public Select where(String column, Select innerSelect) {
-		appendWhereClauseConnector();
-		selectBuilder.append(column);
-		selectBuilder.append(" = (");
-		selectBuilder.append(innerSelect.selectBuilder);
-		selectBuilder.append(")");
-		return this;
 	}
 
 	public Join join(String table) {
 		return new Join(this, table);
 	}
 
-	class Join {
+	public class Join {
+
 		private Select select;
 
 		public Join(Select select, String table) {
 			this.select = select;
-			this.select.selectBuilder.append(" JOIN " + table);
+			selectBuilder.append(" JOIN " + table);
 		}
 
 		public Select on(String leftColumn, String rightColumn) {
-			select.selectBuilder.append(" ON ");
-			select.selectBuilder.append(leftColumn);
-			select.selectBuilder.append(" = ");
-			select.selectBuilder.append(rightColumn);
+			selectBuilder.append(" ON ");
+			selectBuilder.append(leftColumn);
+			selectBuilder.append(" = ");
+			selectBuilder.append(rightColumn);
 			return this.select;
 		}
 	}
 
-	// TODO: add joins
 	// TODO: add QueryOperators for where clause
-	// TODO: add custom where
 	// TODO: add whereIn
 	// TODO: add whereBetween
 	// TODO: add like support (QueryOperators)
@@ -101,28 +120,37 @@ public class Select {
 
 	// when getting rows as map.. it would be convenient to have some map that supports getting value as required type
 	public Map<String, Object> asMap() {
+		appendWhereStatements();
 		logSelect(selectBuilder);
 		return jdbcTemplate.queryForMap(selectBuilder.toString());
 	}
 
 	public List<Map<String, Object>> asList() {
+		appendWhereStatements();
 		logSelect(selectBuilder);
 		return jdbcTemplate.queryForList(selectBuilder.toString());
 	}
 
 	public Boolean asBoolean() {
+		appendWhereStatements();
 		logSelect(selectBuilder);
 		return queryForObject(Boolean.class);
 	}
 
 	public Long asLong() {
+		appendWhereStatements();
 		logSelect(selectBuilder);
 		return queryForObject(Long.class);
 	}
 
 	public String asString() {
+		appendWhereStatements();
 		logSelect(selectBuilder);
 		return queryForObject(String.class);
+	}
+
+	private void appendWhereStatements() {
+		this.selectBuilder.append(whereBuilder);
 	}
 
 	private <T> T queryForObject(Class<T> classObj) {
@@ -131,6 +159,7 @@ public class Select {
 			throw new IllegalArgumentException("Exactly one column must appear in the"
 				+ " select clause to directly ask for its value in this manner.");
 		}
+//		return jdbcTemplate.queryForObject(sql, rowMapper); // could use for StringMap with values converted
 		return jdbcTemplate.queryForObject(selectBuilder.toString(), classObj);
 	}
 

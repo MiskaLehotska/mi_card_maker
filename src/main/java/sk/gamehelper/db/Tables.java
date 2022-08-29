e7f1ab7a0bc7a72c315aa98e867927673a3f73a2
Miska Lehotska <misela.le@gmail.com>
2022-08-29 15:36:33 +0200

    added tables name constant to class Tables

---
+++ src/main/java/sk/gamehelper/db/Tables.java
@@ -0,0 +1,10 @@
+package sk.gamehelper.db;
+
+public class Tables {
+
+	
+	public static final String COIN_ENUM = "card_enum.e_coin";
+	public static final String RARITY_ENUM = "card_enum.e_rarity";
+	public static final String CATEGORY_ENUM = "card_enum.e_category";
+
+}

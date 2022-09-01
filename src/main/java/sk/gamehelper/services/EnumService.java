266c0e2caf84d0dc8d23a6c4ed5a2095d606a686
Miska Lehotska <misela.le@gmail.com>
2022-09-01 12:53:42 +0200

    [CODEREVIEW] EnumService - formatted code

--- src/main/java/sk/gamehelper/services/EnumService.java
+++ src/main/java/sk/gamehelper/services/EnumService.java
@@ -1,39 +1,38 @@
 package sk.gamehelper.services;
 
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import sk.gamehelper.db.Database;
 import sk.gamehelper.db.Tables;
+import sk.gamehelper.helpers.CMap;
 
 @Component
 public class EnumService {
 
-	
 	@Autowired
 	private Database db;
-	
-	public List<Map<String, Object>> getCategoryEnum(){
+
+	public List<CMap> getCategoryEnum(){
 		return db.select()
-				.from(Tables.CATEGORY_ENUM)
-				.where("b_valid")
-				.asList();
+			.from(Tables.CATEGORY_ENUM)
+			.where("b_valid")
+			.asList();
 	}
-	
-	public List<Map<String, Object>> getCoinEnum(){
+
+	public List<CMap> getCoinEnum(){
 		return db.select()
-				.from(Tables.COIN_ENUM)
-				.where("b_valid")
-				.asList();
+			.from(Tables.COIN_ENUM)
+			.where("b_valid")
+			.asList();
 	}
-	
-	public List<Map<String, Object>> getRarityEnum(){
+
+	public List<CMap> getRarityEnum(){
 		return db.select()
-				.from(Tables.RARITY_ENUM)
-				.where("b_valid")
-				.asList();
+			.from(Tables.RARITY_ENUM)
+			.where("b_valid")
+			.asList();
 	}
 }

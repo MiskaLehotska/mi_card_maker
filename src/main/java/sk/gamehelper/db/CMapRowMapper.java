3bc9b9653cc104d78a0227261d2bf8423e3d9ad5
Miska Lehotska <misela.le@gmail.com>
2022-09-01 12:56:01 +0200

    added CMapRowMapper

---
+++ src/main/java/sk/gamehelper/db/CMapRowMapper.java
@@ -0,0 +1,26 @@
+package sk.gamehelper.db;
+
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+
+import sk.gamehelper.helpers.CMap;
+
+@Component
+public class CMapRowMapper implements RowMapper<CMap> {
+
+	@Override
+	public CMap mapRow(ResultSet rs, int rowNum) throws SQLException {
+		ResultSetMetaData rsMeta = rs.getMetaData();
+		int columnCount = rsMeta.getColumnCount();
+		CMap mapOfColumnValues = new CMap();
+		for (int i = 1; i < columnCount; i++) {
+			Object value = rs.getObject(i);
+			mapOfColumnValues.put(rsMeta.getColumnName(i), value);
+		}
+		return mapOfColumnValues;
+	}
+}

3b98b76d48d96c344bd0a6c28a5f485d32136fad
Miska Lehotska <misela.le@gmail.com>
2022-09-06 19:52:08 +0200

    renamed LoadProperties to MessagesLoader and omitted throwing exception

---
+++ src/main/java/sk/gamehelper/helpers/MessagesLoader.java
@@ -0,0 +1,33 @@
+package sk.gamehelper.helpers;
+
+import java.io.IOException;
+import java.util.Properties;
+import java.util.logging.Logger;
+
+public class MessagesLoader {
+
+	private static final Logger logger = Logger.getAnonymousLogger();	
+	private static final Properties property = new Properties();
+
+	static {
+		try {
+			property.load(MessagesLoader.class.getClassLoader().getResourceAsStream("messages.properties"));
+			logger.info("Messages have been loaded.");
+		} catch(IOException e){
+			logger.severe(e.getMessage());
+		}
+	}
+
+	public static String resolveMessage(String propertyName, Object ...objects) {
+		String message = property.getProperty(propertyName);
+		if (message == null) {
+			return propertyName;
+		}
+		if (!(objects.length == 0)) {
+			for (int i = 0; i < objects.length; i++) {
+				message = message.replace("{" + i + "}", objects[i].toString());
+			}
+		}
+		return message;
+	}
+}

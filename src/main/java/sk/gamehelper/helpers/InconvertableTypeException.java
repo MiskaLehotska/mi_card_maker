7bfb324b3bc217060908bf20a3f89e959f67e77c
Miska Lehotska <misela.le@gmail.com>
2022-09-06 18:23:42 +0200

    [DELETE] deleted InconvertableTypeException from helpers package

--- src/main/java/sk/gamehelper/helpers/InconvertableTypeException.java
+++
@@ -1,9 +0,0 @@
-package sk.gamehelper.helpers;
-
-public class InconvertableTypeException extends RuntimeException{
-	
-	public InconvertableTypeException(Object value, Class<?> type) {
-		super( value +  " cannot be converted to " + type);
-	}
-
-}

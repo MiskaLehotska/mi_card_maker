0149fe0c91d818651872aa6e33193b0febcf0e8a
Martin Krajcovic <mar.krajc@gmail.com>
2022-09-09 20:52:39 +0200

    Table - renamed table prefix from t_ to v_ to use db views

--- src/main/java/sk/gamehelper/db/Table.java
+++ src/main/java/sk/gamehelper/db/Table.java
@@ -5,6 +5,6 @@ public class Table {
 	public static final String COIN_ENUM = "e_coin";
 	public static final String RARITY_ENUM = "e_rarity";
 	public static final String CATEGORY_ENUM = "e_category";
-	public static final String MAGIC_ITEM = "t_magic_item";
+	public static final String MAGIC_ITEM = "v_magic_item";
 
 }

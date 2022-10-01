42b302c33d52504a1bb716da861e790adc7e5ef5
Miska Lehotska <misela.le@gmail.com>
2022-10-01 15:45:47 +0200

    [CODEREWIEV][SONAR] added constant for duplicating literal in enum service

--- src/main/java/sk/gamehelper/services/EnumService.java
+++ src/main/java/sk/gamehelper/services/EnumService.java
@@ -15,27 +15,29 @@ import sk.gamehelper.helpers.CMap;
 @Component
 public class EnumService {
 
+	private static final String IS_VALID = "b_valid";
+
 	@Autowired
 	private Database db;
 
 	public List<CMap> getCategoryEnum(){
 		return db.select()
 			.from(CATEGORY_ENUM)
-			.where("b_valid")
+			.where(IS_VALID)
 			.asList();
 	}
 
 	public List<CMap> getCoinEnum(){
 		return db.select()
 			.from(COIN_ENUM)
-			.where("b_valid")
+			.where(IS_VALID)
 			.asList();
 	}
 
 	public List<CMap> getRarityEnum(){
 		return db.select()
 			.from(RARITY_ENUM)
-			.where("b_valid")
+			.where(IS_VALID)
 			.asList();
 	}
 }

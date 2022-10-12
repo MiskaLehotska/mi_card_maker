f7e0d126f7052f9698cf0bd5ccfce88548e34a01
Miska Lehotska <misela.le@gmail.com>
2022-10-12 20:26:32 +0200

    added test after db load

--- src/test/java/sk/gamehelper/db/InitialLoadTest.java
+++ src/test/java/sk/gamehelper/db/InitialLoadTest.java
@@ -1,5 +1,10 @@
 package sk.gamehelper.db;
 
+import java.time.LocalDateTime;
+import java.time.chrono.ChronoLocalDate;
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -8,6 +13,7 @@ import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import sk.gamehelper.config.AppConfig;
+import sk.gamehelper.helpers.CMap;
 
 @TestInstance(Lifecycle.PER_CLASS)
 class InitialLoadTest {
@@ -25,5 +31,21 @@ class InitialLoadTest {
 	void loadTest() {
 		// TODO: dorobit test na to, ci v den zapisu novych dat do databazy sa tam 
 		// nachadzaju naozaj len tie, ktorych t_write nie je skor ako den tohto noveho loadu
+		//mam db
+		Database db = context.getBean(Database.class);
+		
+		LocalDateTime before = LocalDateTime.of(2022, 10, 11, 00, 00);
+		
+		List<CMap> list = db.select("t_write").
+				from("t_magic_item")
+				.asList();
+	
+		long count = list.stream()
+				.map(e -> e.getLocalDateTime("write"))
+				.filter(time -> time.isBefore(before))
+				.count();
+		
+		Assertions.assertEquals(0, count);
+	
 	}
 }
